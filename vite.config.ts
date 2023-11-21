cecb9c0b9f47105305eaded0ed8880850731462c
Kim Minh Thang <thangved02@gmail.com>
2023-11-21 11:50:15 +0700

    feat: optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), ViteMinifyPlugin()],
+	plugins: [react(), ViteMinifyPlugin(), ViteImageOptimizer()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
