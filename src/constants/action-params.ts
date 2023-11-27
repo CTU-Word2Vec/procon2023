7bf3a01714da3312608c117e16041e334d693a79
Kim Minh Thang <thangved02@gmail.com>
2023-11-27 22:17:02 +0700

    fix: order of move params

--- src/constants/action-params.ts
+++ src/constants/action-params.ts
@@ -3,7 +3,14 @@ export type EMoveParam = EBaseActionParam | 'UP' | 'DOWN' | 'UPPER_LEFT' | 'UPPE
 export type EBuildDestryParam = EBaseActionParam | 'ABOVE' | 'BELOW';
 export type EActionParam = EMoveParam | EBuildDestryParam;
 
+/**
+ * @description Build or destroy action params (ABOVE, RIGHT, BELOW, LEFT)
+ */
 export const buildDestroyActionParams: EBuildDestryParam[] = ['ABOVE', 'RIGHT', 'BELOW', 'LEFT'];
+
+/**
+ * @description Move action params (UP, DOWN, LEFT, RIGHT, UPPER_LEFT, UPPER_RIGHT, LOWER_LEFT, LOWER_RIGHT)
+ */
 export const moveParams: EMoveParam[] = [
 	'UP',
 	'DOWN',
