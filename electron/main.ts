4c94c45a4a1dd7384622e1a3997ba38b9b60e9d0
Kim Minh Thang <thangved02@gmail.com>
2023-11-27 11:10:10 +0700

    ci: release

--- electron/main.ts
+++ electron/main.ts
@@ -3,7 +3,12 @@ import { app, BrowserWindow } from 'electron';
 app.whenReady().then(() => {
 	const win = new BrowserWindow({
 		title: 'Procon 2023 - Castle game',
+		webPreferences: {
+			devTools: false,
+		},
 	});
+
+	win.menuBarVisible = false;
 
 	// You can use `process.env.VITE_DEV_SERVER_URL` when the vite command is called `serve`
 	if (process.env.VITE_DEV_SERVER_URL) {
