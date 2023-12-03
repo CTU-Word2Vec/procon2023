bddaff6df188757b27412a7b1d8b84da40982290
Kim Minh Thang <thangved02@gmail.com>
2023-12-03 19:40:12 +0700

    chore: mock test

--- src/__tests__/game/classes/CaroGameManager.test.ts
+++ src/__tests__/game/classes/CaroGameManager.test.ts
@@ -1,76 +1,7 @@
+import * as gameField from '@/__tests__/mocks/games/1.json';
 import { CaroGameManager } from '@/game/classes';
 import Field from '@/models/Field';
 import { describe, expect, it } from 'vitest';
-
-const gameField = {
-	castle_coeff: 20,
-	castles: [
-		{
-			x: 5,
-			y: 3,
-		},
-		{
-			x: 6,
-			y: 6,
-		},
-	],
-	craftsmen: [
-		{
-			x: 2,
-			y: 2,
-			id: '0',
-			side: 'A',
-		},
-		{
-			x: 7,
-			y: 4,
-			id: '1',
-			side: 'A',
-		},
-		{
-			x: 9,
-			y: 5,
-			id: '2',
-			side: 'B',
-		},
-		{
-			x: 3,
-			y: 4,
-			id: '3',
-			side: 'B',
-		},
-	],
-	height: 10,
-	id: 0,
-	lastTurn: 17,
-	match_id: 0,
-	name: 'Random Field',
-	ponds: [
-		{
-			x: 7,
-			y: 8,
-		},
-		{
-			x: 8,
-			y: 5,
-		},
-		{
-			x: 3,
-			y: 2,
-		},
-		{
-			x: 2,
-			y: 1,
-		},
-		{
-			x: 2,
-			y: 4,
-		},
-	],
-	territory_coeff: 5,
-	wall_coeff: 1,
-	width: 10,
-};
 
 describe('CaroGameManager', () => {
 	it('game field should be defined', () => {
