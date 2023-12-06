3e4b1f7097344d683dfe813800b95bf3daa3d339
Kim Minh Thang <thangved02@gmail.com>
2023-12-06 21:08:44 +0700

    chore: remove enemy wall weight

--- src/game/classes/CaroGameManager.ts
+++ src/game/classes/CaroGameManager.ts
@@ -161,7 +161,7 @@ export default class CaroGameManager extends GameManager implements ICaroGameMan
 						const p = new Position(i, j);
 						if (!this.hashedWalls.exist(p)) continue;
 						if (this.hashedWalls.read(p)!.side === side) this.scoreCounter.increase(pos, 0.125);
-						else this.scoreCounter.decrease(pos, 0.125);
+						// else this.scoreCounter.decrease(pos, 0.125);
 					}
 				}
 			}
